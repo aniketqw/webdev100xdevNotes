5f30833b0e0874439666c31d7d860e1fa2b92b91
aniketqw <aniket21stcentury@gmail.com>
2025-04-20 18:52:26 +0530

    Important comment to make any function Promisified function

--- 2/2.5/readFilePromisifiedWithCatch.js
+++ 2/2.5/readFilePromisifiedWithCatch.js
@@ -1,7 +1,8 @@
 
 const fs=require('fs');
 function readFileAsync(file){
-    return new Promise(function(resolve,reject){
+    return new Promise(function(resolve,reject){//this function should contain the function like readfile , settimeout etc for which we want to promisify
+        //AND resolve and reject are the function which will be called when the promise is resolved or rejected
         fs.readFile(file,'utf-8',function(err,data){//basically whatever we write inside resolve and reject will be delivered to the data field of anonymous function (see line 20) inside then function
             if(err){
                 //reject the promise
