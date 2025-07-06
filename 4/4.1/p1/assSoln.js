73fe76888d2d5d7071971d7dda9776d12002fc8a
aniketqw <aniket21stcentury@gmail.com>
2025-07-06 23:33:23 +0530

    using commander as we can add multiple command here we have added count coommand

--- 4/4.1/p1/assSoln.js
+++ 4/4.1/p1/assSoln.js
@@ -1,17 +1,50 @@
 // this is the solution implemented by harkirat in video
 
-const fs =require('fs');
+// const fs =require('fs');
 
-function main(fileName){
-    console.log(process.argv);
-    fs.readFile(fileName,"utf-8",function(err,data){
-        let total =0;
-        for(let i=0;i<data.length;i++)
-        {
-            if(data[i]===" ")
-                total++;
-        }
-        console.log(total+1);
-    })
-}
-main(process.argv[2]);
+// function main(fileName){
+//     console.log(process.argv);
+//     fs.readFile(fileName,"utf-8",function(err,data){
+//         let total =0;
+//         for(let i=0;i<data.length;i++)
+//         {
+//             if(data[i]===" ")
+//                 total++;
+//         }
+//         console.log(total+1);
+//     })
+// }
+// main(process.argv[2]);
+
+
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
+
+program.command('count')
+  .description('Count the number of words in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const words = data.split(' ').length;
+        // console.log(data.split(' '));
+        console.log(`There are ${words} words in ${file}`);
+        
+      }
+    });
+  });
+
+program.parse();
+// to get count
+// node assSoln.js count /Users/aniketsaxena/Documents/p/p0/dump/webDev/100xdevCohort3/liveClasses/webdev/webdev100xdevNotes/4/4.1/p1/a.txt
+
+//to get a help 
+// node assSoln.js -h 
